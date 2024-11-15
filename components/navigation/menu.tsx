8252ee81e080de644901252f14b405b7d2d32685
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-11-15 15:52:36 +0100

    nextjs template

--- components/navigation/menu.tsx
+++ components/navigation/menu.tsx
@@ -17,7 +17,7 @@ const Menu = () => {
         </li>
         <li>
           <Link
-            href="/products"
+            href="#"
             className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Shop
@@ -25,7 +25,7 @@ const Menu = () => {
         </li>
         <li>
           <Link
-            href="promotions"
+            href="#"
             className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Promotions
@@ -33,7 +33,7 @@ const Menu = () => {
         </li>
         <li>
           <Link
-            href="/contact"
+            href="#"
             className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
           >
             Contact
